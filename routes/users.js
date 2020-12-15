213e083a3c68d5cbca48af03f719fa8d824e4ff0
stvenduong <stvenduong@gmail.com>
2020-12-14 18:00:14 -0800

    Authenticated route with JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -45,9 +47,25 @@ router.post(
 
       await user.save();
 
-      res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (error) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
-      console.log(err.message);
+      console.err(err.message);
       res.status(500).send("Server Error");
     }
   }
