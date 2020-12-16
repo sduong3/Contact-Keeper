935f966873c74b305c2acdc1359dd35891b35593
stvenduong <stvenduong@gmail.com>
2020-12-15 20:18:16 -0800

    Added Delete Contact implemetation (from UI)

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import ContactContext from "../../context/contact/contactContext";
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact } = contactContext;
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+  };
 
   return (
     <div className='card bg-light'>
@@ -31,7 +38,9 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <button className='btn btn-dark btn-sm'>Edit</button>
-      <button className='btn btn-danger btn-sm'>Delete</button>
+      <button className='btn btn-danger btn-sm' onClick={onDelete}>
+        Delete
+      </button>
     </div>
   );
 };
